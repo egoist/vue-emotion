25a815b65e3b8a63242d8c7fe8ce1140ed42b9e2
u3u <qwq@qwq.cat>
2018-04-23 14:04:34 +0800

    Export everything from emotion

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,8 +1,1 @@
-import { IntrinsicElementAttributes } from 'vue-tsx-support/types/dom'
-
-declare module 'vue-emotion' {
-  const styled: IntrinsicElementAttributes
-  export default styled
-  export const css: any
-  export const injectGlobal: any
-}
+export * from 'emotion'
