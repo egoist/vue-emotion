76549e2a10026db0ba367b10b20900b3c9d13888
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2022-12-29 15:00:04 +0530

    vue 3 support

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import createCache from '@emotion/cache'
+import createCache from "@emotion/cache";
 
-export { styled } from './styled'
-export { createGlobalStyle } from './global'
+export { styled } from "./styled";
+export { createGlobalStyle } from "./global";
 
 export const VueEmotion = {
   beforeCreate() {
-    this.$emotionCache = (this.$parent && this.$parent.$emotionCache) || createCache({ key: 'css' })
+    this.$emotionCache =
+      (this.$parent && this.$parent.$emotionCache) ||
+      createCache({ key: "css" });
   },
-}
+};
